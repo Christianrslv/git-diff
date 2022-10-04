d799603e3890982a2ed0029e8c8b95604b2e71b8
Christianrslv <christianrslv22@gmail.com>
2022-10-04 16:00:36 -0300

    First version

---
+++ index.js
@@ -0,0 +1,19 @@
+import { getInput, setFailed, setOutput } from '@actions/core';
+import { context } from '@actions/github';
+import { createRequire } from 'node:module';
+ 
+const require = createRequire(import.meta.url);
+const fs = require('fs');
+const yml = require('js-yaml');
+
+try {
+  let file = getInput('config-file');
+  if(file == '') file = 'config.deploy.yml';
+  const fileContents = fs.readFileSync(file, 'utf8');
+  const data = yml.load(fileContents);
+  setOutput('content', data);
+  const payload = JSON.stringify(context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    setFailed(error.message);
+}
